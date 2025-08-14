8eeaeafd018fc76c28fcb167d5fb977a48eb5e95
mzamoraboza <mzamoraboza@gmail.com>
2025-08-14 14:57:28 -0600

    Update GestionEstudiante.java
    MANUEL ZAMORA BOZA 14 08 2025

--- proyectofinalPOO/src/main/java/sc202jng6/proyectofinalpoo/GestionEstudiante.java
+++ proyectofinalPOO/src/main/java/sc202jng6/proyectofinalpoo/GestionEstudiante.java
@@ -1,40 +1,48 @@
 package sc202jng6.proyectofinalpoo;
-
+/**
+ *
+ * @author Manuel Zamora Boza
+ */
 import javax.swing.JOptionPane;
 
 public class GestionEstudiante {
+    private Estudiante[] estudiantes = new Estudiante[100];
+    private int contador = 0;
 
-    // Atributos de la gestión del estudiantes
-    Estudiante[] estudiantes = new Estudiante[100]; // Array para almacenar estudiantes
-    int contador = 0; // Contador para el número de estudiantes
-    // Método para agregar un estudiante
-
-    public void agregarEstudiante(Estudiante estudiante) { // Método para agregar un estudiante al arreglo
-        if (contador < estudiantes.length) {
-            estudiantes[contador] = estudiante;
-            contador++;
-        } else {
-            JOptionPane.showMessageDialog(null, "No se pueden agregar más estudiantes.", "Error",
-                    JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    public GestionEstudiante() { // Constructor para inicializar algunos estudiantes
+    public GestionEstudiante() {
         estudiantes[contador++] = new Estudiante(1, "Juan Pérez", "estudiante123");
         estudiantes[contador++] = new Estudiante(2, "Ana Gómez", "estudiante123");
-        estudiantes[contador++] = new Estudiante(3, "Luis Fernández", "estudiante123");
-        estudiantes[contador++] = new Estudiante(4, "María López", "estudiante123");
     }
 
-    public void mostrarEstudiantes() { // Método para mostrar la información de todos los estudiantes
-        String informacionEstudiantes = "Estudiantes:\n";
-        informacionEstudiantes += "ID\tNombre\tCorreo\n";
+    public void menuEstudiantes() {
+        String[] opciones = {"Mostrar Estudiantes", "Agregar Estudiante", "Volver"};
+        int opcion;
+        do {
+            opcion = JOptionPane.showOptionDialog(null, "Gestión de Estudiantes",
+                    "Estudiantes", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                    null, opciones, opciones[0]);
+            switch (opcion) {
+                case 0 -> mostrarEstudiantes();
+                case 1 -> agregarEstudiante();
+            }
+        } while (opcion != 2);
+    }
+
+    public void mostrarEstudiantes() {
+        StringBuilder info = new StringBuilder("ID\tNombre\n");
         for (int i = 0; i < contador; i++) {
-            informacionEstudiantes += estudiantes[i].getId() + "\t" +
-                    estudiantes[i].getNombre() + "\t" + "\n";
+            info.append(estudiantes[i].getId()).append("\t")
+                    .append(estudiantes[i].getNombreEstudiante()).append("\n");
         }
-        JOptionPane.showMessageDialog(null, informacionEstudiantes, "Lista de Estudiantes",
+        JOptionPane.showMessageDialog(null, info.toString(), "Lista de Estudiantes",
                 JOptionPane.INFORMATION_MESSAGE);
     }
 
+    public void agregarEstudiante() {
+        int id = contador + 1;
+        String nombre = JOptionPane.showInputDialog("Nombre del Estudiante:");
+        String contrasena = JOptionPane.showInputDialog("Contraseña:");
+        estudiantes[contador++] = new Estudiante(id, nombre, contrasena);
+        JOptionPane.showMessageDialog(null, "Estudiante agregado correctamente.");
+    }
 }
