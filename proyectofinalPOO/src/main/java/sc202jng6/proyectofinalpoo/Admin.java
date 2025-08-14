93ff62bc04362c3005b1db65dda961f9507e3f46
mzamoraboza <mzamoraboza@gmail.com>
2025-08-14 14:51:01 -0600

    Update Admin.java
    Manuel Zamora Boza 14/08/2025

--- proyectofinalPOO/src/main/java/sc202jng6/proyectofinalpoo/Admin.java
+++ proyectofinalPOO/src/main/java/sc202jng6/proyectofinalpoo/Admin.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author mzamo
+ * @author Manuel Zamora Boza
  */
 
 public class Admin {
@@ -13,36 +13,42 @@ public class Admin {
     private String nombre;
     private String contrasena;
     private String tipoUsuario = "Administrador";
+
     // Constructor
     public Admin(int id, String nombre, String contrasena) {
         this.id = id;
         this.nombre = nombre;
         this.contrasena = contrasena;
     }
-    //getters
+
+    // getters
     public int getId() {
         return id;
-    }   
+    }
+
     public String getNombre() {
         return nombre;
     }
-    
+
     public String getContrasena() {
         return "*".repeat(contrasena.length());
     }
+
     public String getTipoUsuario() {
         return tipoUsuario;
-    }   
-    //setters
+    }
+
+    // setters
     public void setId(int id) {
         this.id = id;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+
     public void setContrasena(String contrasena) {
-       boolean isValid = true;
+        boolean isValid = true;
 
         if (contrasena.length() < 8) {
             JOptionPane.showMessageDialog(null, "La contraseña debe tener al menos 8 caracteres.", "Error",
@@ -63,22 +69,21 @@ public class Admin {
                 this.contrasena = contrasena;
                 JOptionPane.showMessageDialog(null, "Contraseña actualizada correctamente.", "Éxito",
                         JOptionPane.INFORMATION_MESSAGE);
-                        
+
             }
         }
     }
+
     public void setTipoUsuario(String tipoUsuario) {
         this.tipoUsuario = tipoUsuario;
     }
-   
+
     public void mostrarInformacionAdmin() {
         JOptionPane.showMessageDialog(null,
-                "ID: " + id + "\nNombre: " + nombre +"\nTipo de Usuario: " + tipoUsuario,
+                "ID: " + id + "\nNombre: " + nombre + "\nTipo de Usuario: " + tipoUsuario,
                 "Información del Administrador", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    
-   
-
-    
 }
+
+
